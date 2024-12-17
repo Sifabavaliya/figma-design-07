5c3b505b9b48cf2463c4262f185a7e2edf8e9df4
Sifa Bavaliya <sifabavaliya205@gmail.com>
2024-12-17 13:59:48 +0530

    update popup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
 // Handle toggle menu click.
 
-function showMenu(){
-    const mainMenu=document.querySelector('#main-menu');
-    mainMenu.style.display='flex';
+function showMenu() {
+  const mainMenu = document.querySelector("#main-menu");
+  mainMenu.style.display = "flex";
 }
 
-function closeMenu(){
-    const mainMenu=document.querySelector('#main-menu');
-    mainMenu.style.display='none';
+function closeMenu() {
+  const mainMenu = document.querySelector("#main-menu");
+  mainMenu.style.display = "none";
 }
+
+// handle popup
+document.getElementById("close-btn").addEventListener("click", (e) => {
+  document.getElementById("popup").classList.add("hide");
+});
+
+setTimeout(() => {
+  document.getElementById("popup").classList.add("show");
+}, 4000);
